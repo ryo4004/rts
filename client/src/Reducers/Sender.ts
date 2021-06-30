5adbc32c4517674d4d280f8a7f0aac33e4974a90
ryo4004 <kato@ryo4004.net>
2021-07-01 00:47:03 +0900

    Senderに型を追加

--- client/src/Reducers/Sender.ts
+++ client/src/Reducers/Sender.ts
@@ -1,20 +1,21 @@
-const initialState = {
+import { ACTION_TYPE } from '../Actions/Sender'
+import type { Actions } from '../Actions/Sender'
+
+type SenderState = {
+  loading: boolean
+  sendFileList: object
+}
+
+const initialState: SenderState = {
   loading: false,
 
   // 追加されたファイルと状態の管理
   sendFileList: {},
 }
 
-const prefix = 'SENDER_'
-
-export default function senderReducer(state = initialState, action) {
+export default function senderReducer(state = initialState, action: Actions): SenderState {
   switch (action.type) {
-    case prefix + 'LOADING':
-      return {
-        ...state,
-        loading: action.payload.loading,
-      }
-    case prefix + 'SET_SEND_FILE_LIST':
+    case ACTION_TYPE.setSendFileList:
       return {
         ...state,
         sendFileList: action.payload.sendFileList,
